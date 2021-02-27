4a44cb37c96bac70690fa4f4ac5c157f571b4c3a
Morad <morad.jamhour@gmail.com>
2021-02-27 04:22:57 +0100

    Completed

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// What's your name
+var name = prompt("Write your name");
+
+// What's your last name
+var surname = prompt("Write your surname");
+
+// What's your favourite color
+var color = prompt("What is your favourite color?")
+
+// Print the result
+document.getElementById('password').innerHTML = "Your new super safe and unhackable password is: " + name + surname + color + "21";
